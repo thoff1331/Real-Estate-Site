37f09f203a58eb53af309838aad39dfe5af6b76e
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-09-23 12:52:28 -0600

    login is finished

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -13,22 +13,44 @@ import Navbar from '../Navbar/Navbar';
 
 class Login extends Component {
 constructor(props) {
-super(props);    
+super(props);   
+this.state = {
+ email: "",
+ passWord: "" 
+};
+this.handleChange = this.handleChange.bind(this);
+this.handleSubmit = this.handleSubmit.bind(this);
 }
 componentDidMount() {
+    console.log(this.state.email);
 
 }
+
+handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleSubmit() {
+ if (this.state.passWord != "123" || this.state.email != "1@gmail.com" ) {
+alert("Inccorrect Credentials!") }
+ else {
+  alert("Success")
+    this.props.history.push("/create");
+ 
+}
+console.log("hit");   
+ }
+  
 render() {
  return (
 <div>
 <Navbar />
     <div className='login-box'>
     <h4>Sign In to continue</h4>
-   <input placeholder='Email' />
+   <input placeholder='Email' name='email' onChange={this.handleChange} value={this.state.email}/>
    <br />
-   <input placeholder='Password' />
+   <input placeholder='Password' name='passWord' onChange={this.handleChange} value={this.state.passWord} type='password' />
    <br />
-   <button>Login</button>
+   <button onClick={this.handleSubmit}>Login</button>
    <br />
    <button> Forgot Password?</button> 
    </div>
