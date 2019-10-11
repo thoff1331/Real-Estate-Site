58019d848bc92a4b0a81d8cd6a115ce4688a6883
Trevor hoffman <trevorhoffman@Trevors-MacBook-Air.local>
2019-10-11 13:53:20 -0600

    contact and home edited

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "../Navbar/Navbar";
-
+import "../Contact/Contact.css";
 class Contact extends Component {
   constructor(props) {
     super(props);
@@ -10,8 +10,20 @@ class Contact extends Component {
     return (
       <div>
         <NavBar />
-        <div>
-          <h1>Contact me Page </h1>
+        <div className="contact-info">
+          <h1>Contact me </h1>
+          <img src="https://www.cherylbarrustooelerealestate.com/images/agent.png" />
+          <a href="435-840-4466" data-rel="external">
+            Phone: 435-840-4466
+          </a>
+          <h3>Fax: (435) 884-3900 </h3>
+          <a
+            href="cheryl@realtypath.com
+"
+            data-rel="external"
+          >
+            Email: cheryl@realtypath.com
+          </a>
         </div>
       </div>
     );
